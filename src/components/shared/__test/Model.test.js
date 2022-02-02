422be83b58d9850c78989cb36b13eea049c907cb
shbhm6496 <96365905+shbhm6496@users.noreply.github.com>
2022-02-02 08:49:40 +0530

    Update Model.test.js

--- src/components/shared/__test/Model.test.js
+++ src/components/shared/__test/Model.test.js
@@ -5,9 +5,3 @@ test("should first", () => {
   render(<ModalForm />);
   screen.debug();
 });
-
-test("Shoulh Firt", () => {
-  render(<ModalForm props={((subject = "subject"), (message = "message"))} />);
-  expect(screen.getByText("hello")).toBeDefined();
-  ex;
-});
