5b539b40c7362660175e071a540fa233a8eaf415
Zuber Shekh <zuber.shekh7@gmail.com>
2022-02-14 20:36:03 +0530

    update user dashboard

--- src/pages/users/DashboardPage.js
+++ src/pages/users/DashboardPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Container, Row, Col } from "react-bootstrap";
-import LinkCard from "../../components/shared/LinkCard";
+import { Link } from "react-router-dom";
 
 const UserDashboardPage = ({ history }) => {
   const { userInfo } = useSelector((state) => state.userLogin);
@@ -13,50 +12,40 @@ const UserDashboardPage = ({ history }) => {
   }, [userInfo, history]);
 
   return (
-    <main className="mt-4">
-      <Container>
+    <main className="mt-10">
+      <section className="px-10">
+        <h1 className="text-4xl font-bold mb-3">Your Account</h1>
         {userInfo && (
-          <Row>
-            <Col md={8} className="mx-auto">
-              <h1>Your Account</h1>
-              <section className="mt-3">
-                <Row>
-                  <Col md={6}>
-                    <LinkCard
-                      title="Your Orders"
-                      text="Track or buy things again"
-                      link="/users/orders"
-                    />
-                  </Col>
-                  <Col md={6}>
-                    <LinkCard
-                      title="Your Profile"
-                      text="Edit name, email or mobile number"
-                      link="/users/profile/"
-                    />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={6}>
-                    <LinkCard
-                      title="Your Addresses"
-                      text="Edit addresses for orders"
-                      link="/users/addresses"
-                    />
-                  </Col>
-                  <Col md={6}>
-                    <LinkCard
-                      title="Your Wishlist"
-                      text="Explore wishlist or buy things"
-                      link="/users/wishlist"
-                    />
-                  </Col>
-                </Row>
-              </section>
-            </Col>
-          </Row>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-3 gap-y-2">
+            <div className="px-3 py-4 border-2 rounded-lg span-col-1 mb-3 hover:bg-gray-100">
+              <Link to="/users/orders">
+                <h4 className="text-2xl font-medium">Your Orders</h4>
+                <h6>Track or buy things again</h6>
+              </Link>
+            </div>
+            <div className="px-3 py-4 border-2 rounded-lg span-col-1 mb-3 hover:bg-gray-100">
+              <Link to="/users/profile/">
+                <h4 className="text-2xl font-medium">Your Profile</h4>
+                <h6>Edit name, email or mobile number</h6>
+              </Link>
+            </div>
+
+            <div className="px-3 py-4 border-2 rounded-lg span-col-1 mb-3 hover:bg-gray-100">
+              <Link to="/users/addresses">
+                <h4 className="text-2xl font-medium">Your Addresses</h4>
+                <h6>Edit addresses for orders</h6>
+              </Link>
+            </div>
+
+            <div className="px-3 py-4 border-2 rounded-lg span-col-1 mb-3 hover:bg-gray-100">
+              <Link to="/users/wishlist">
+                <h4 className="text-2xl font-medium">Your Wishlist</h4>
+                <h6>Explore wishlist or buy things</h6>
+              </Link>
+            </div>
+          </div>
         )}
-      </Container>
+      </section>
     </main>
   );
 };
