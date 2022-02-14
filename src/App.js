487f422f4f29ffffdf6819f09791fcef6f994de1
Zuber Shekh <zuber.shekh7@gmail.com>
2022-02-14 19:41:51 +0530

    remove non-user pages from app.js

--- src/App.js
+++ src/App.js
@@ -9,44 +9,15 @@ import Footer from "./components/shared/Footer";
 import LoginPage from "./pages/users/LoginPage";
 import SignupPage from "./pages/users/SignupPage";
 import UserDashboardPage from "./pages/users/DashboardPage";
-import SellerHomePage from "./pages/sellers/HomePage";
 import UserProtectedRoute from "./components/routes/UserProtectedRoute";
-import SellerLoginPage from "./pages/sellers/LoginPage";
-import SellerSignupPage from "./pages/sellers/SignupPage";
-import AdminLoginPage from "./pages/admin/LoginPage";
-import SellerDashboardPage from "./pages/sellers/DashboardPage";
 import UserProfilePage from "./pages/users/ProfilePage";
 import EditUserProfilePage from "./pages/users/EditProfilePage";
-import AdminDashboardPage from "./pages/admin/DashboardPage";
-import SellerProtectedRoute from "./components/routes/SellerProtectedRoute";
-import AdminProtectedRoute from "./components/routes/AdminProtectedRoute";
-import CreateBusinessPage from "./pages/sellers/CreateBusinessPage";
-import BusinessDetailPage from "./pages/sellers/BusinessDetailPage";
-import CategoriesPage from "./pages/categories/CategoriesPage";
-import CategoryPage from "./pages/categories/CategoryPage";
-import AddCategoryPage from "./pages/categories/AddCategoryPage";
-import EditCategoryPage from "./pages/categories/EditCategoryPage";
-import AddProductPage from "./pages/products/AddProductPage";
-import ProductPage from "./pages/products/ProductPage";
-import EditProductPage from "./pages/products/EditProductPage";
-import EditBusinessPage from "./pages/business/EditBusinessPage";
 import BusinessPage from "./pages/business/BusinessPage";
 import WishListPage from "./pages/wishlist/WishListPage";
 import AddressesPage from "./pages/addresses/AddressesPage";
 import AddAddressPage from "./pages/addresses/AddAddressPage";
 import EditAddressPage from "./pages/addresses/EditAddressPage";
 import AddressPage from "./pages/addresses/AddressPage";
-
-import BusinessCategoriesPage from "./pages/businessCategories/BusinessCategoriesPage";
-import BusinessCategoryPage from "./pages/businessCategories/BusinessCategoryPage";
-import EditBusinessCategoryPage from "./pages/businessCategories/EditBusinessCategoryPage";
-import AddBusinessCategoryPage from "./pages/businessCategories/AddBusinessCategoryPage";
-
-import ManageCategoryAdminPage from "./pages/admin/ManageCategoryAdminPage";
-import ManageProductAdminPage from "./pages/admin/ManageProductAdminPage";
-import ManageSellerAdminPage from "./pages/admin/ManageSellerAdminPage";
-import ManageUsersAdminPage from "./pages/admin/ManageUsersAdminPage";
-import ManageAdminListPage from "./pages/admin/ManageAdminListPage";
 import UserCategoryPage from "./pages/categories/users/CategoryPage";
 import UserProductPage from "./pages/products/users/ProductPage";
 import CartPage from "./pages/cart/CartPage";
@@ -55,8 +26,6 @@ import PaymentMethodPage from "./pages/checkout/PaymentMethodPage";
 import OrderSummaryPage from "./pages/checkout/OrderSummaryPage";
 import UserOrdersPage from "./pages/orders/UserOrdersPage";
 import UserOrderPage from "./pages/orders/UserOrderPage";
-import SellerOrdersPage from "./pages/orders/SellerOrdersPage";
-import SellerOrderPage from "./pages/orders/SellerOrderPage";
 
 const App = () => {
   return (
@@ -77,7 +46,7 @@ const App = () => {
             component={UserProductPage}
           />
           <Route path="/business/:business_id" component={BusinessPage} />
-          {/* users */}
+          {/* user */}
           <Route path="/users/login" component={LoginPage} exact />
           <Route path="/users/signup" component={SignupPage} exact />
           <UserProtectedRoute
@@ -142,120 +111,6 @@ const App = () => {
           <UserProtectedRoute
             path="/users/orders/:order_id"
             component={UserOrderPage}
-          />
-          {/* sellers */}
-          <Route path="/sellers/" component={SellerHomePage} exact />
-          <Route path="/sellers/login" component={SellerLoginPage} exact />
-          <Route path="/sellers/signup" component={SellerSignupPage} exact />
-          <SellerProtectedRoute
-            path="/sellers/dashboard"
-            component={SellerDashboardPage}
-            exact
-          />
-          <SellerProtectedRoute
-            path="/sellers/business/new"
-            component={CreateBusinessPage}
-            exact
-          />
-          <SellerProtectedRoute
-            path="/sellers/business"
-            component={BusinessDetailPage}
-            exact
-          />
-          <SellerProtectedRoute
-            path="/sellers/manage/business/:business_id/edit"
-            component={EditBusinessPage}
-          />
-          <SellerProtectedRoute
-            path="/sellers/manage/categories"
-            component={CategoriesPage}
-            exact
-          />
-          <SellerProtectedRoute
-            path="/sellers/manage/categories/new"
-            component={AddCategoryPage}
-            exact
-          />
-          <SellerProtectedRoute
-            path="/sellers/manage/categories/:category_id/edit"
-            component={EditCategoryPage}
-            exact
-          />
-          <SellerProtectedRoute
-            path="/sellers/manage/categories/:category_id/products/new"
-            component={AddProductPage}
-            exact
-          />
-          <SellerProtectedRoute
-            path="/sellers/manage/categories/:category_id"
-            component={CategoryPage}
-          />
-          <SellerProtectedRoute
-            path="/sellers/manage/products/:product_id/edit"
-            component={EditProductPage}
-          />
-          <SellerProtectedRoute
-            path="/sellers/manage/products/:product_id"
-            component={ProductPage}
-          />
-          <SellerProtectedRoute
-            path="/manage/orders/"
-            component={SellerOrdersPage}
-            exact
-          />
-          <SellerProtectedRoute
-            path="/manage/orders/:order_id"
-            component={SellerOrderPage}
-          />
-          {/* admin */}
-          <Route path="/admin/login" component={AdminLoginPage} exact />
-          <AdminProtectedRoute
-            path="/admin/account"
-            component={AdminDashboardPage}
-            exact
-          />
-          <Route
-            path="/admin/category"
-            component={ManageCategoryAdminPage}
-            exact
-          />
-          <AdminProtectedRoute
-            path="/admin/categories/:category_id"
-            component={ManageProductAdminPage}
-            exact
-          />
-          <AdminProtectedRoute
-            path="/admin/sellers"
-            component={ManageSellerAdminPage}
-            exact
-          />
-          <AdminProtectedRoute
-            path="/admin/users"
-            component={ManageUsersAdminPage}
-            exact
-          />
-          <AdminProtectedRoute
-            path="/admin/admin"
-            component={ManageAdminListPage}
-            exact
-          />
-          <AdminProtectedRoute
-            path="/manage/business-categories"
-            component={BusinessCategoriesPage}
-            exact
-          />
-          <AdminProtectedRoute
-            path="/manage/business-categories/new"
-            component={AddBusinessCategoryPage}
-            exact
-          />
-          <AdminProtectedRoute
-            path="/manage/business-categories/:category_id/edit"
-            component={EditBusinessCategoryPage}
-          />
-          <AdminProtectedRoute
-            path="/manage/business-categories/:category_id"
-            component={BusinessCategoryPage}
           />
           {/* 404 */}
           <Route path="*" component={NotFoundPage} />
--- src/pages/core/NotFoundPage.js
+++ src/pages/core/NotFoundPage.js
@@ -1,27 +1,24 @@
 import React from "react";
-import { Image, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import NotFoundImage from "../../assets/images/404.png";
 
 const NotFoundPage = () => {
   return (
-    <main className="mt-4 text-center">
-      <h1 className="display-5">
+    <main className="mt-10 flex flex-col items-center">
+      <h1 className="text-2xl lg:text-4xl">
         Look like you are lost, let me help to go home
       </h1>
-      <Container>
-        <Image
-          fluid
-          style={{ height: 300 }}
-          src={NotFoundImage}
-          alt="404 Not Found"
-        />
-      </Container>
-      <section>
-        <Link className="btn btn-primary" to="/">
-          <Button size="lg">Way To Home</Button>
+
+      <img className="w-80" src={NotFoundImage} alt="404 Not Found" />
+
+      <div className="mt-5">
+        <Link
+          className="py-3 px-3 bg-indigo-500 text-white rounded-lg text-bold text-lg hover:text-indigo-400 transition duration-300"
+          to="/"
+        >
+          Way to Home
         </Link>
-      </section>
+      </div>
     </main>
   );
 };
