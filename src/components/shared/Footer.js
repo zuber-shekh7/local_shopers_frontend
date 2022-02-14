919c039a44d25e9171640fa3957672896f247b58
Zuber Shekh <zuber.shekh7@gmail.com>
2022-02-14 20:14:58 +0530

    update footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <footer className="bg-light py-4">
-      <Container>
-        <Row>
-          <Col className="mx-auto" md={6}>
-            <section>
-              <h4 className="text-center">Copyright &copy; 2022</h4>
-            </section>
-          </Col>
-        </Row>
-      </Container>
+    <footer className="bg-gray-200 py-10 mt-10">
+      <section className="flex justify-center">
+        <h4 className="text-lg text-center">Copyright &copy; 2022</h4>
+      </section>
     </footer>
   );
 };
