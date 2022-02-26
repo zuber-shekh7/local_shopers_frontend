1b86a3b2899b0a07718c1651fec34fae510f620d
Zuber Shekh <zuber.shekh7@gmail.com>
2022-02-26 19:46:51 +0530

    add pulse loader to addresses page

--- src/pages/addresses/AddressesPage.js
+++ src/pages/addresses/AddressesPage.js
@@ -29,35 +29,68 @@ const AddressesPage = () => {
               Add address
             </Link>
           </div>
-
-          {addresses && addresses.length && addresses.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
-              {addresses.map((address) => {
+          {!addresses && loading && (
+            <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-3">
+              {[...Array(6).fill(1, 6)].map((value, index) => {
                 return (
                   <div
-                    key={address._id}
-                    className="bg-gray-50 border-2 rounded-lg px-4 py-4 shadow-lg"
+                    key={index + 1}
+                    className="border border-gray-300 shadow-lg rounded-lg p-4 md:max-w-sm w-full mx-auto"
                   >
-                    <h2 className="text-2xl font-medium">{address.fullName}</h2>
-                    <p>
-                      {address.flatNo} {address.street} {address.landmark}
-                    </p>
-                    <p>
-                      {address.city}, {address.state}, {address.pincode}
-                    </p>
-                    <p>Phone Number: {address.mobileNumber}</p>
-
-                    <div className="inline-block py-2 px-4 bg-indigo-500 rounded-lg text-white mt-3">
-                      <Link to={`/users/addresses/${address._id}`}>View</Link>
+                    <div className="animate-pulse flex space-x-4">
+                      <div className="flex-1 space-y-3 py-1">
+                        <div className="h-2 bg-gray-500 rounded"></div>
+                        <div className="space-y-3">
+                          <div className="grid grid-cols-1 gap-3">
+                            <div className="h-3 w-6/12 bg-gray-500 rounded col-span-1"></div>
+                            <div className="h-3 w-5/12 bg-gray-500 rounded col-span-1"></div>
+                            <div className="h-3 w-4/12 bg-gray-500 rounded col-span-1"></div>
+                          </div>
+                          <div className="h-8 w-3/12 bg-gray-500 rounded-lg"></div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 );
               })}
             </div>
-          ) : (
-            <div className="flex justify-center">
-              <h3 className="text-2xl text-center">No address available</h3>
-            </div>
+          )}
+          {addresses && (
+            <>
+              {addresses.length > 0 ? (
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+                  {addresses.map((address) => {
+                    return (
+                      <div
+                        key={address._id}
+                        className="bg-gray-50 border-2 rounded-lg px-4 py-4 shadow-lg"
+                      >
+                        <h2 className="text-2xl font-medium">
+                          {address.fullName}
+                        </h2>
+                        <p>
+                          {address.flatNo} {address.street} {address.landmark}
+                        </p>
+                        <p>
+                          {address.city}, {address.state}, {address.pincode}
+                        </p>
+                        <p>Phone Number: {address.mobileNumber}</p>
+
+                        <div className="inline-block py-2 px-4 bg-indigo-500 rounded-lg text-white mt-3">
+                          <Link to={`/users/addresses/${address._id}`}>
+                            View
+                          </Link>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <div className="flex justify-center">
+                  <h3 className="text-2xl text-center">No address available</h3>
+                </div>
+              )}
+            </>
           )}
         </div>
       </section>
