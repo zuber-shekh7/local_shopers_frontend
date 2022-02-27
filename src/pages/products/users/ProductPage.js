69f10729b06d948b1cf1e58666c6b33dbfa16f58
Zuber Shekh <zuber.shekh7@gmail.com>
2022-02-27 20:46:21 +0530

    add pulse loader to the product page

--- src/pages/products/users/ProductPage.js
+++ src/pages/products/users/ProductPage.js
@@ -15,7 +15,7 @@ const UserProductPage = ({ match, history }) => {
 
   useEffect(() => {
     dispatch(getProduct(product_id));
-  }, []);
+  }, [product_id, dispatch]);
 
   const addToCartHandler = () => {
     history.push(`${routes.cart}/${product_id}?quantity=${quantity}`);
@@ -27,6 +27,26 @@ const UserProductPage = ({ match, history }) => {
 
   return (
     <main>
+      <section className="m-10 px-10 max-w-6xl mx-auto">
+        {loading && !product && (
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-x-10">
+            <div className="animate-pulse col-span-6 p-10 ">
+              <div className="flex justify-center items-center">
+                <div className="block h-72 w-full bg-gray-300 rounded-lg"></div>
+              </div>
+            </div>
+            <div className="animate-pulse col-span-6 p-10 ">
+              <div className="h-12 w-4/12 bg-gray-300 rounded-lg mb-3"></div>
+              <div className="h-8 w-4/12 bg-gray-300 rounded-lg mb-3"></div>
+              <div className="h-4 w-5/12 bg-gray-300 rounded-lg mb-3"></div>
+              <div className="h-10 w-full bg-gray-300 rounded-lg mb-3"></div>
+              <div className="h-10 w-full bg-gray-300 rounded-lg mb-3"></div>
+              <div className="h-10 w-full bg-gray-300 rounded-lg mb-3"></div>
+            </div>
+          </div>
+        )}
+      </section>
+
       <section className="m-10 px-10 max-w-6xl mx-auto">
         {product && (
           <div className="grid grid-cols-1 md:grid-cols-12 gap-x-10">
