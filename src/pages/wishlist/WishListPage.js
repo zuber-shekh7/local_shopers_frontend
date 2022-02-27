ccac16b95718593fbd5b092777d1300fc8c3b881
Zuber Shekh <zuber.shekh7@gmail.com>
2022-02-27 21:03:09 +0530

    add pulse loader to whishlist page

--- src/pages/wishlist/WishListPage.js
+++ src/pages/wishlist/WishListPage.js
@@ -9,7 +9,8 @@ import {
 } from "../../actions/wishListActions";
 
 const WishListPage = ({ match }) => {
-  const { loading, wishList, error } = useSelector((state) => state.wishList);
+  const loading = true;
+  const { wishList, error } = useSelector((state) => state.wishList);
 
   const { user } = useSelector((state) => state.userLogin);
 
@@ -35,46 +36,70 @@ const WishListPage = ({ match }) => {
     <main>
       <section className="m-10 max-w-6xl mx-auto px-10">
         <h1 className="text-4xl font-semibold mb-4">Your Wish List</h1>
-        <div>
-          {wishList && wishList.products && wishList.products.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-              {wishList.products.map((product) => {
-                return (
-                  <div
-                    key={product._id}
-                    className="bg-gray-50 border-2 rounded-lg px-4 py-4 shadow-lg"
-                  >
-                    <h2 className="text-2xl font-semibold mb-3">
-                      {product.name}
-                    </h2>
-                    <p className="mb-3">{product.description}</p>
-                    <div className="flex space-x-2">
-                      <Link
-                        className="bg-indigo-500 px-3 py-2 text-white rounded-lg"
-                        to={`/business/products/${product._id}`}
-                      >
-                        View More
-                      </Link>
-
-                      <button
-                        className="bg-red-500 px-3 py-2 text-white rounded-lg"
-                        onClick={() =>
-                          handleRemoveFromList(wishList._id, product._id)
-                        }
-                      >
-                        Remove
-                      </button>
-                    </div>
+        {loading && !wishList && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+            {[...Array(6).fill(1, 6)].map((value, index) => {
+              return (
+                <div
+                  key={index}
+                  className="bg-gray-50 border-2 rounded-lg px-4 py-4 shadow-lg"
+                >
+                  <div className="h-8 w-8/12 bg-gray-300 rounded-lg mb-3"></div>
+                  <div className="h-4 w-8/12 bg-gray-300 rounded-lg mb-3"></div>
+                  <div className="flex space-x-2">
+                    <div className="h-8 w-4/12 bg-gray-300 rounded-lg "></div>
+                    <div className="h-8 w-4/12 bg-gray-300 rounded-lg "></div>
                   </div>
-                );
-              })}
-            </div>
-          ) : (
-            <div className="flex justify-center">
-              <h3 className="text-2xl text-center">
-                No product available in wish list
-              </h3>
-            </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+
+        <div>
+          {wishList && (
+            <>
+              {wishList.products.length > 0 ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+                  {wishList.products.map((product) => {
+                    return (
+                      <div
+                        key={product._id}
+                        className="bg-gray-50 border-2 rounded-lg px-4 py-4 shadow-lg"
+                      >
+                        <h2 className="text-2xl font-semibold mb-3">
+                          {product.name}
+                        </h2>
+                        <p className="mb-3">{product.description}</p>
+                        <div className="flex space-x-2">
+                          <Link
+                            className="bg-indigo-500 px-3 py-2 text-white rounded-lg"
+                            to={`/business/products/${product._id}`}
+                          >
+                            View More
+                          </Link>
+
+                          <button
+                            className="bg-red-500 px-3 py-2 text-white rounded-lg"
+                            onClick={() =>
+                              handleRemoveFromList(wishList._id, product._id)
+                            }
+                          >
+                            Remove
+                          </button>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <div className="flex justify-center">
+                  <h3 className="text-2xl text-center">
+                    No product available in wish list
+                  </h3>
+                </div>
+              )}
+            </>
           )}
         </div>
       </section>
