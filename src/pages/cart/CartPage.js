cadf29ccfe5b34eb01ddbc3babe38a5ce2291b2f
Zuber Shekh <zuber.shekh7@gmail.com>
2022-02-28 18:56:26 +0530

    fix add to cart function bug

--- src/pages/cart/CartPage.js
+++ src/pages/cart/CartPage.js
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { addToCart, removeFromCart } from "../../actions/cartActions";
 
 const CartPage = ({ match, location, history }) => {
+  console.log(match.params);
   const { product_id } = match.params;
 
   const quantity = location.search ? Number(location.search.split("=")[1]) : 1;
@@ -46,37 +47,39 @@ const CartPage = ({ match, location, history }) => {
                 <table className="table-auto w-full">
                   <thead className="text-left">
                     <tr className="border-b-2">
-                      <th className="text-2xl py-2">Products</th>
-                      <th className="text-2xl">Price</th>
-                      <th className="text-2xl">Quantity</th>
-                      <th className="text-2xl">Total</th>
+                      <th className="text-lg md:text-2xl py-2">Products</th>
+                      <th className="text-lg md:text-2xl">Price</th>
+                      <th className="text-lg md:text-2xl">Quantity</th>
+                      <th className="text-lg md:text-2xl">Total</th>
                     </tr>
                   </thead>
                   <tbody>
                     {cartItems.map((item) => {
                       return (
-                        <tr className="border-b">
+                        <tr key={item._id} className="border-b">
                           <td>
-                            <div className="flex py-5 space-x-10 px-5">
+                            <div className="flex py-5 space-x-5 md:space-x-10 px-5">
                               <img
-                                className=" h-24 w-24 rounded-lg"
+                                className="h:8 w-8 md:h-24 md:w-24 rounded-lg object-cover"
                                 src={item.image}
                                 alt={item.name}
                               />
-                              <Link to="/">
-                                <h2 className="text-2xl text-indigo-500">
+                              <Link
+                                to={`/business/products/${item.product._id}`}
+                              >
+                                <h2 className="text-lg md:text-2xl text-indigo-500">
                                   {item.name}
                                 </h2>
                               </Link>
                             </div>
                           </td>
 
-                          <td className="text-xl text-indigo-500">
+                          <td className="text-lg md:text-xl text-indigo-500">
                             ₹ {item.price}/-
                           </td>
                           <td>
                             <select
-                              className="py-2 rounded-lg outline-none mr-5"
+                              className="text-sm  md:text-lg py-2 rounded-lg outline-none mr-5"
                               value={item.qty}
                               onChange={(e) => {
                                 dispatch(
@@ -101,13 +104,13 @@ const CartPage = ({ match, location, history }) => {
                           <td className="text-xl">
                             <div className="flex justify-between">
                               <span>₹ {item.price * item.qty}/-</span>
-                              <div className="flex items-start">
+                              <div className="flex items-center">
                                 <button
                                   onClick={() =>
                                     removeFromCartHandler(item._id)
                                   }
                                 >
-                                  <HiOutlineXCircle className="text-gray-500 h-8 w-8" />
+                                  <HiOutlineXCircle className="text-gray-500 h-4 w-4 md:h-8 md:w-8" />
                                 </button>
                               </div>
                             </div>
@@ -119,11 +122,11 @@ const CartPage = ({ match, location, history }) => {
                 </table>
                 <div className="flex justify-end bg-gray-200 px-5 py-5">
                   <div>
-                    <h6 className="text-2xl font-medium mb-3">
+                    <h6 className="text-lg md:text-2xl font-medium mb-3">
                       Sub Total of{" "}
                       {cartItems.reduce((acc, item) => acc + item.qty, 0)} items
                     </h6>
-                    <h4 className="text-xl font-semibold mb-3">
+                    <h4 className="text-lg md:text-xl font-semibold mb-3">
                       ₹{" "}
                       {cartItems
                         .reduce((acc, item) => acc + item.price * item.qty, 0)
@@ -133,7 +136,7 @@ const CartPage = ({ match, location, history }) => {
                     <button
                       onClick={checkoutHandler}
                       disabled={cartItems.length === 0}
-                      className=" text-lg w-full py-2 bg-indigo-500 rounded-lg text-white"
+                      className="text-base md:text-lg w-full px-2 py-2 bg-indigo-500 rounded-lg text-white"
                     >
                       Proceed To Checkout
                     </button>
