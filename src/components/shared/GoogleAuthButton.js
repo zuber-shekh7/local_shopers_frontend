45dded067b24f04dd137973477f82bb6750f1cb6
Zuber Shekh <zuber.shekh7@gmail.com>
2022-03-09 17:11:51 +0530

    update style of google auth button

--- src/components/shared/GoogleAuthButton.js
+++ src/components/shared/GoogleAuthButton.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import { FaGoogle } from "react-icons/fa";
 
 const GoogleAuthButton = (props) => {
   return (
-    <Button
+    <button
       onClick={props.onClick}
       disabled={props.disabled}
-      className="flex justify-center  items-center space-x-2 px-3 py-2 bg-indigo-500 w-full rounded-lg text-white mb-3"
+      className="flex justify-center items-center space-x-2 px-3 py-2 bg-indigo-600 w-full rounded-lg text-white hover:bg-indigo-700"
     >
       <FaGoogle className="h-5 w-5" />
       <span>Continue with Google</span>
-    </Button>
+    </button>
   );
 };
 
