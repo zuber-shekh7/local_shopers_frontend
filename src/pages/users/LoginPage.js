84da7b3fe763c3531655549a04b7c857236d8e6d
Zuber Shekh <zuber.shekh7@gmail.com>
2022-04-02 17:54:51 +0530

    change design of LoginPage

--- src/pages/users/LoginPage.js
+++ src/pages/users/LoginPage.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { GoogleLogin } from "react-google-login";
-import { HiOutlineShoppingBag } from "react-icons/hi";
 import { userLogin, userLoginWithGoogle } from "../../actions/userActions";
 import GoogleAuthButton from "../../components/shared/GoogleAuthButton";
-
 import routes from "../../utils/routes";
+import { Logo } from "../../components/logos";
+import { Input, Label } from "../../components/forms/inputs";
+import { Button } from "../../components/buttons";
+import { FormGroup } from "../../components/forms/containers";
+import { Loader } from "../../components/loaders";
+import { Error } from "../../components/messages";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -41,49 +45,40 @@ const LoginPage = () => {
   return (
     <main className="bg-indigo-600">
       <section className="flex flex-col justify-center items-center h-screen">
-        <div className="w-11/12 sm:w-96 bg-white rounded-xl shadow-lg p-10">
-          <Link to={routes.home}>
-            <h2 className="flex gap-x-1 text-3xl text-indigo-600">
-              <HiOutlineShoppingBag className="h-8 w-8" />
-              <span className="font-bold ">Local Shoppers</span>
-            </h2>
-          </Link>
-
+        <div className="w-11/12 sm:w-96 bg-white rounded-xl shadow-lg px-10 py-5">
+          <Logo />
           <h1 className="my-4">Login</h1>
           <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="email">Email</label>
-              <input
-                className="w-full text-lg py-2 px-2 rounded-lg border-2 border-indigo-600  focus:ring-indigo-600"
+            <FormGroup>
+              <Label htmlFor="email">Email*</Label>
+              <Input
+                className="w-full"
                 type="email"
                 id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                placeholder="seller@example.com"
+                placeholder="Enter your email"
                 required
               />
-            </div>
-            <div className="mb-5">
-              <label htmlFor="password">Password</label>
-              <input
-                className="w-full text-lg py-2 px-2 rounded-lg border-2 border-indigo-600  focus:ring-indigo-600"
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="password">Password*</Label>
+              <Input
+                className="w-full"
                 type="password"
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                placeholder="don't share your password"
+                placeholder="Enter your password"
                 required
               />
-            </div>
-            <div className="mb-5">
-              <button
-                className="btn text-lg bg-indigo-600 w-full border hover:bg-indigo-700"
-                type="submit"
-              >
+            </FormGroup>
+            <FormGroup>
+              <Button className="w-full" type="submit">
                 Login
-              </button>
-            </div>
-            <div className="mb-5">
+              </Button>
+            </FormGroup>
+            <FormGroup>
               <GoogleLogin
                 clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}
                 buttonText="Contine with Google"
@@ -92,21 +87,21 @@ const LoginPage = () => {
                 onFailure={responseGoogle}
                 cookiePolicy={"single_host_origin"}
               />
-            </div>
-            <div className="mb-5 text-center">
-              {!loading && error && <p className="text-red-500">{error}</p>}
-              {loading && <p>Loading...</p>}
-            </div>
+            </FormGroup>
+            <FormGroup className="flex justify-center mb-0">
+              {error && <Error>{error}</Error>}
+              {loading && <Loader />}
+            </FormGroup>
           </form>
         </div>
         <div className="mt-5">
-          <p className="text-center text-white text-xl">
+          <p className="text-center text-white">
             Don't have an account?{" "}
             <Link className="underline" to={routes.signup}>
               Signup
             </Link>
           </p>
-          <p className="text-center text-white text-xl">
+          <p className="text-center text-white">
             <Link className="underline" to={routes.forgotPassword}>
               Forgot Password?
             </Link>
