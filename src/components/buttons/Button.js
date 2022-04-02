b68a16a9051ea03e626047bbc6fc194662d4438e
Zuber Shekh <zuber.shekh7@gmail.com>
2022-04-02 15:39:45 +0530

    add Button to shared component

---
+++ src/components/buttons/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({ onClick, children, className, ...rest }) => {
+  return (
+    <button
+      className={`bg-indigo-600 text-white px-4 py-2 rounded ${className}`}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
