d1511e07de65330a8752c3f2dc91954276f0da91
Zuber Shekh <zuber.shekh7@gmail.com>
2022-04-05 18:40:28 +0530

    add support for headers based token authentication

--- src/apis/backendAPI.js
+++ src/apis/backendAPI.js
@@ -2,9 +2,14 @@ import axios from "axios";
 
 const BACKEND_SERVICE_URL = process.env.REACT_APP_BACKEND_SERVER_URL;
 
+const token = `Bearer ${JSON.parse(localStorage.getItem("token"))}`;
+
 const backendAPI = axios.create({
   baseURL: `${BACKEND_SERVICE_URL}`,
   withCredentials: true,
+  headers: {
+    Authorization: token,
+  },
 });
 
 export default backendAPI;
